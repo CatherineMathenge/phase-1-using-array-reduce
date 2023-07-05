3c746509f5fef4db242ce764a32acb844e17dc9a
Catherine <catherine.mathenge@strathmore.edu>
2023-07-05 12:21:31 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 const totalBatteries = batteryBatches.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 
-module.exports = totalBatteries;
+
